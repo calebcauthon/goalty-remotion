2bfb596855a07b0cd92655d051d3c9e58ddeb427
caleb cauthon <caleb.cauthon@pricefx.com>
2024-10-27 00:38:26 -0500

    Update video seeking functionality to use the current player frame instead of the provided frame.

--- react_app/src/components/VideoSeeker.js
+++ react_app/src/components/VideoSeeker.js
@@ -2,11 +2,13 @@ import { useCallback } from 'react';
 
 export function useVideoSeeker({ playerRef }, currentFrame) {
   const seekBackward = useCallback(() => {
-    playerRef.current?.seekTo(Math.max(currentFrame - 5, 0));
+    const currentPlayerFrame = playerRef.current?.getCurrentFrame();
+    playerRef.current?.seekTo(Math.max(currentPlayerFrame - 5, 0));
   }, [currentFrame, playerRef]);
 
   const seekForward = useCallback(() => {
-    playerRef.current?.seekTo(currentFrame + 5);
+    const currentPlayerFrame = playerRef.current?.getCurrentFrame();
+    playerRef.current?.seekTo(currentPlayerFrame + 5);
   }, [currentFrame, playerRef]);
 
   return { seekBackward, seekForward };
