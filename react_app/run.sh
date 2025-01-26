6034d7c4f479810ed4971141cd3fc24a109d8d13
caleb cauthon <caleb.cauthon@pricefx.com>
2025-01-26 08:37:56 -0600

    Add reminders and notifications to run environment setup in run.sh script.

--- react_app/run.sh
+++ react_app/run.sh
@@ -1,2 +1,6 @@
 nvm use 18
+echo "🔧 Remember to run 'nvm use 18' if you haven't already! 🚀"
+
+
+echo "🚀 Running: export \$(cat .react.local | xargs) && npm run start"
 export $(cat .react.local | xargs) && npm run start
